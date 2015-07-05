816751fcf0dd4105c26c3d472cdd3cc0b1a25ecf
Turash Jurij <yrik.turash@gmail.com>
2015-07-05 23:12:51 +0300

    2: Create models & generated data

---
+++ binary-linq/binary-linq/Models.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace binary_linq
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Year { get; set; }
+        public string City { get; set; }
+        public string Univercity { get; set; }
+        public string Category { get; set; }
+    }
+    public class Test
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public List<Question> Questions { get; set; }
+        public int MaxPassTime { get; set; }
+        public int PassMark { get; set; }
+    }
+    public class Question
+    {
+        public string Text { get; set; }
+        public string Category { get; set; }
+    }
+    public class TestWork
+    {
+        public Test Test { get; set; }
+        public User User { get; set; }
+        public int Result { get; set; }
+        public int PassTime { get; set; }
+    }
+}
--- binary-linq/binary-linq/Program.cs
+++ binary-linq/binary-linq/Program.cs
@@ -10,6 +10,153 @@ namespace binary_linq
     {
         static void Main(string[] args)
         {
+            #region GenerateData
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Name = "Mercedes Sutton",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = ".Net"
+                },
+                new User()
+                {
+                    Name = "Bradley	Harvey",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = "JS"
+                },
+                new User()
+                {
+                    Name = "Roman Cobb",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = "PHP"
+                },
+                new User()
+                {
+                    Name = "Jack Stanley",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = "DB"
+                },
+                new User()
+                {
+                    Name = "Stuart Freeman",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = "OOP"
+                },
+                new User()
+                {
+                    Name = "Kim	Bowman",
+                    Univercity = "KPI",
+                    Year = "1993",
+                    City = "Lviv",
+                    Category = "English"
+                },
+            };
+            var questions = new List<Question>()
+            {
+                new Question()
+                {
+                    Text = "ddfref",
+                    Category = "English"
+                },
+                new Question()
+                {
+                    Text = "ddfref",
+                    Category = "English"
+                },
+                new Question()
+                {
+                    Text = "ddfref",
+                    Category = ".Net"
+                },
+                new Question()
+                {
+                    Text = "ddfref",
+                    Category = ".Net"
+                },
+                new Question()
+                {
+                    Text = "ddfref",
+                    Category = ".Net"
+                },
+            };
+            var tests = new List<Test>()
+            {
+                new Test()
+                {
+                    Name = "English Test",
+                    Category = "English",
+                    Questions = questions.Where(n => n.Category == "English").ToList(),
+                    MaxPassTime = 30,
+                    PassMark = 60
+                },
+                new Test()
+                {
+                    Name = ".Net Test",
+                    Category = ".Net",
+                    Questions = questions.Where(n => n.Category == ".Net").ToList(),
+                    MaxPassTime = 25,
+                    PassMark = 65
+                }
+            };
+            var testWorks = new List<TestWork>()
+            {
+                new TestWork()
+                {
+                    User = users.ElementAt(0),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 80,
+                    PassTime = 29
+                },
+                new TestWork()
+                {
+                    User = users.ElementAt(1),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 88,
+                    PassTime = 25
+                },
+                new TestWork()
+                {
+                    User = users.ElementAt(2),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 95,
+                    PassTime = 30
+                },
+                new TestWork()
+                {
+                    User = users.ElementAt(3),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 98,
+                    PassTime = 25
+                },
+                new TestWork()
+                {
+                    User = users.ElementAt(4),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 56,
+                    PassTime = 26
+                },
+                new TestWork()
+                {
+                    User = users.ElementAt(5),
+                    Test = tests.Single(n => n.Name == "English Test"),
+                    Result = 44,
+                    PassTime = 13
+                },
+            };
+#endregion
+
+
         }
     }
 }
